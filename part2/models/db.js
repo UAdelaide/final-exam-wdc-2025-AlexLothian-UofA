33d5fb7a2e6889f2d6559bd5ccbd5a16a12f2634
a-lothian <alexlothian0@gmail.com>
2025-06-20 06:25:02 +0000

    [automatic commit] 2025-06-20 06:25:02 UTC 101.185.78.49

--- part2/models/db.js
+++ part2/models/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: 'dogwalks.sql',
+  database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
