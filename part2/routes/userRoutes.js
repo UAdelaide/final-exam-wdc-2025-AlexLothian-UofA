db5e8181b188e0f72ec1e3ca21ca60d55bab37fb
a-lothian <alexlothian0@gmail.com>
2025-06-20 09:17:04 +0000

    [automatic commit] 2025-06-20 09:17:04 UTC 101.185.78.49

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -57,9 +57,9 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', async (req, res) => {
   if (req.session) {
-    
+    req.session.destroy()
   }
-  
+  req.session.destroy(() => {});
 })
 
 module.exports = router;
