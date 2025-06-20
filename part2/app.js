7f51d98476e1509ea43d5b7c582586833eba0088
a-lothian <alexlothian0@gmail.com>
2025-06-20 06:31:07 +0000

    [automatic commit] 2025-06-20 06:31:07 UTC ;; communications error to 208.67.222.222#53: timed out 101.185.78.49

--- part2/app.js
+++ part2/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
-
+app.use(morgan('dev'));
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
