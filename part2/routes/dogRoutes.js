3335f1c8c846a8396212f5968f8402630cede25e
a-lothian <alexlothian0@gmail.com>
2025-06-21 04:23:34 +0000

    [automatic commit] 2025-06-21 04:23:34 UTC ;; communications error to 208.67.222.222#53: timed out 101.185.78.49

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,7 +5,9 @@ const db = require('../models.db');
 router.get('/', async (req, res) => {
     try {
         const [rows] = await db.query(
-            `SELECT dog_id, name, size `
+            `SELECT dog_id, name, size, owner_id
+            FROM Dogs
+            ORDER BY dog_id`
         )
     }
 })
